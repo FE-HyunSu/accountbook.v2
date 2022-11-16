626cc828a760ef99ff22ecc42a7d69e5fa21bbce
FE.hyunsu <fe.hyunsu@gmail.com>
2022-11-16 21:18:44 +0900

    add : sample page. (Observer sample page)

---
+++ components/sample/item/style.tsx
@@ -0,0 +1,65 @@
+import styled, { keyframes } from 'styled-components';
+
+const viewMotion = keyframes`
+  0%{transform:translate(-70px) scale(1, .3); opacity:0;}
+  70%{transform:translate(10px) scale(1, .3); opacity:1;}
+  100%{transform:translate(0px) scale(1, 1); opacity:1;}
+`;
+
+export const AccountCard = styled.dl`
+  display: flex;
+  position: relative;
+  width: 100%;
+  border-top: 1px solid #f4f4f4;
+  font-size: 1.4rem;
+  transition: 0.2s;
+  box-sizing: border-box;
+  opacity: 0;
+  &.active {
+    animation: ${viewMotion} 0.3s forwards linear alternate;
+  }
+  &:hover {
+    padding: 0 1rem;
+    background-color: #f4f4f4;
+    transform: scale(1.05);
+    @media only screen and (max-width: 992px) {
+      padding: 0;
+      background-color: #fff;
+      transform: scale(1);
+    }
+  }
+  dt {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    strong {
+      display: inline-block;
+      font-weight: 400;
+      color: #3a3a3a;
+      letter-spacing: -0.05rem;
+    }
+    span {
+      display: inline-block;
+      padding-right: 1.5rem;
+      font-weight: 500;
+      font-size: 1.2rem;
+      color: #777;
+    }
+  }
+  dd {
+    flex: 1 1;
+    padding: 2rem;
+    font-weight: 400;
+    text-align: right;
+    &.plus {
+      color: #0a7ec6;
+      &:before {
+        content: '+';
+      }
+    }
+    &.minus {
+      color: #000;
+    }
+  }
+`;
---
+++ pages/sample.tsx
@@ -0,0 +1,11 @@
+import SamplePage from '../components/sample/list/index';
+
+const Sample = () => {
+  return (
+    <>
+      <SamplePage />
+    </>
+  );
+};
+
+export default Sample;
