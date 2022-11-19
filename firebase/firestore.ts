103e9f2bbea2651ec186c26f0746e28520925b42
FE.hyunsu <fe.hyunsu@gmail.com>
2022-11-19 19:07:33 +0900

    modify : firebase.js -> ts.

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -1,11 +1,11 @@
 import { database } from './firebaseConfig';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 
-const getData = async (collectionName) => {
+const getData = async (collectionName: string) => {
   return await getDocs(collection(database, collectionName));
 };
 
-const setData = async (collectionName, data) => {
+const setData = async (collectionName: string, data: object) => {
   return await addDoc(collection(database, collectionName), data);
 };
 
