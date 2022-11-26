1c635efd093349080abb5cf839abab134882f51d
fe.hyunsu <fe.hyunsu@gmail.com>
2022-11-26 19:56:14 +0900

    modify : theme update.

--- components/layout/header/style.tsx
+++ components/layout/header/style.tsx
@@ -1,5 +1,4 @@
-import styled from 'styled-components';
-import { media } from '../../../styles/theme';
+import styled from "styled-components";
 
 export const HeaderBox = styled.header`
   display: block;
